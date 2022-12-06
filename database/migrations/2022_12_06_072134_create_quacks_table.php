063fcc315f5f34401d9ccfada70ae48042d33dac
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-12-06 14:49:03 +0100

    Data Migrations

--- database/migrations/2022_12_06_072134_create_quacks_table.php
+++ database/migrations/2022_12_06_072134_create_quacks_table.php
@@ -13,15 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('quacks', function (Blueprint $table) {
             $table->id();
-            $table->string('firstName',191);
-            $table->string('lastName',191);
-            $table->string('duchname',191);
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('content');
+            $table->string('image',191);
+            $table->string('tags',191);
             $table->timestamps();
         });
     }
@@ -33,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('quacks');
     }
 };
