ea9c09b76c3a38de888336715ad37682029ef4a7
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-12-07 16:41:58 +0100

    fix uploads image

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row g-0 bg-light position-relative">
     <div class="col-md-6 mb-md-0 p-md-4">
-        <img src="{{asset('storage/app/public/uploads/' . $quack->image)}}" class="img-responsive">
+        <img src="{{asset('storage/uploads/' . $quack->image)}}" width="100%">
     </div>
     <div class="col-md-6 p-4 ps-md-0">
     {{$quack->content}}
